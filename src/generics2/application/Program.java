2648af1063fbde19f06ad51fe3d14f768c8b0be4
allamymp <allamympereira@gmail.com>
2023-03-15 07:00:01 -0300

    First Solution

--- src/generics2/application/Program.java
+++ src/generics2/application/Program.java
@@ -1,27 +1,49 @@
 package generics2.application;
 
+import generics2.entities.Product;
+import generics2.services.CalculationService;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
 
 
     public static void main(String[] args) {
 
-        Path path = Paths.get(System.getProperty("user.dir"), "src", "generics2", "FileExercise.csv");
+        Path path = Paths.get("C:\\Programacao\\Workspace\\exercicios_4\\src\\generics2\\FileExercise.csv");
 
         String pathStr = path.toAbsolutePath().toString();
 
+        List<Product> productsList = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(pathStr))) {
+
+            String line = br.readLine();
+            while(line != null){
+                String[] atribute = line.split(",");
+                productsList.add(new Product(atribute[0],Double.parseDouble(atribute[1])));
+                System.out.println(productsList);
+                line= br.readLine();
+
+
+            }
 
 
         }
         catch (IOException e){
             System.out.println("Error "+e.getMessage());
         }
+
+        Product mostExpensive = CalculationService.calculateExpensive(productsList);
+        System.out.println("MAX:");
+        System.out.println(mostExpensive.toString());
+
     }
 
 
--- src/generics2/services/CalculationService.java
+++ src/generics2/services/CalculationService.java
@@ -1,5 +1,30 @@
 package generics2.services;
 
-public class CalculationService<T> {
+import generics2.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculationService<T>   {
+
+    private List<Product> products;
+
+
+    public static Product calculateExpensive(List<Product> products ) {
+
+        if (products.isEmpty()) {
+            throw new IllegalStateException("Empty list!");
+        }
+
+        Product max = products.get(0);
+        for (Product price : products) {
+            if (price.getPrice() > max.getPrice()) {
+                max.setPrice(price.getPrice());
+                max.setName(price.getName());
+            }
+
+        }
+        return max;
+    }
 
 }
